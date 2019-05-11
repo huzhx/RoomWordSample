eac17789545abcc0ddd7ccb6b0a65e610ac7e9d1
Zhaoxian Hu <huzhx@uci.edu>
2019-05-10 17:01:02 -0700

    add Word class

---
+++ app/src/main/java/org/dialysafe/mobile/roomwordsample/Word.java
@@ -0,0 +1,16 @@
+package org.dialysafe.mobile.roomwordsample;
+
+import androidx.annotation.NonNull;
+
+public class Word {
+
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+}
